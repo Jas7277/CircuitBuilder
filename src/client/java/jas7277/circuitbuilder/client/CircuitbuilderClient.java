bb50ef02afdaef28f485ef470d6fc09d1cd8a2a2
Jas7277 <jasonsworld21@yahoo.com>
2024-11-16 18:40:42 -0500

    Initial commit

---
+++ src/client/java/jas7277/circuitbuilder/client/CircuitbuilderClient.java
@@ -0,0 +1,10 @@
+package jas7277.circuitbuilder.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CircuitbuilderClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/jas7277/circuitbuilder/Circuitbuilder.java
@@ -0,0 +1,10 @@
+package jas7277.circuitbuilder;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Circuitbuilder implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
